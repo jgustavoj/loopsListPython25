37e2f93ca1ea465a6e3d11cbbac651da86855089
jgustavoj <gjime011@fiu.edu>
2020-10-19 23:05:20 +0000

    initial commit

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,9 @@
 #Remember import random function here:
+import random
 
 my_list = [4,5,734,43,45]
 
 #The magic is here:
+for i in range(0,10):
+    my_list.append(random.randint())
+print(my_list)
