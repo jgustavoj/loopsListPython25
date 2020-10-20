cb325b57bb7ffb43f8e412f712e7361d8556119f
jgustavoj <gjime011@fiu.edu>
2020-10-20 01:03:06 +0000

    6

--- exercises/04-mixed_list/app.py
+++ exercises/04-mixed_list/app.py
@@ -1,3 +1,8 @@
 mix = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 # Your code below:
+def types():
+    for i in mix: 
+        print(type(i))
+
+types()
--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,5 +1,10 @@
 arr = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 #Your code go here:
-
-
+def sumOdds():
+    total = 0
+    for num in arr: 
+        if num % 2 != 0:
+            total = total + num
+    return total   
+print(sumOdds())
