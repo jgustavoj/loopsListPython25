6282803260a75337d1c64708fec94fe7813c7c7c
jgustavoj <gjime011@fiu.edu>
2020-10-23 22:30:25 +0000

    13.1

--- exercises/13.1-Filter_and_list/app.py
+++ exercises/13.1-Filter_and_list/app.py
@@ -2,6 +2,9 @@
 all_names = ["Romario","Boby","Roosevelt","Emiliy", "Michael", "Greta", "Patricia", "Danzalee"]
 
 #Your code go here:
+def my_funtion(names):
+    return names.startswith("R")
+resulting_names = list(filter(my_funtion, all_names))
 
 print(resulting_names)
 
