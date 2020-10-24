7d9be2a1029875dea1d51b84e6991ab66a2286be
jgustavoj <gjime011@fiu.edu>
2020-10-24 00:57:20 +0000

    15.1

--- exercises/15.1-Matrix_Builder/app.py
+++ exercises/15.1-Matrix_Builder/app.py
@@ -1,3 +1,14 @@
 #Import random
 
 #Create the function below:
+def matrix_builder(integer):
+    list1 = []
+    for i in range(integer):
+        list1.append([])
+        for n in range(integer):
+            list1[i].append(1)
+    print(list1)
+matrix_builder(5)
+
+        
+       
--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,6 +1,16 @@
 
-
+def lyrics_generator(list1):
+    lyric = ""
+    for i in range(len(list1)):
+        if list1[i] == 1 and list1[i-1] == 1 and list1[i-2] == 1:
+            lyric += "Drop the base !!!Break the base!!!"
+        elif list1[i] == 0:
+            lyric += "Boom"
+        else:
+            lyric += "Drop the base"
+    return lyric
 # Your code go above, nothing to change after this line:
+
 print(lyrics_generator([0,0,1,1,0,0,0]))
 print(lyrics_generator([0,0,1,1,1,0,0,0]))
 print(lyrics_generator([0,0,0]))
